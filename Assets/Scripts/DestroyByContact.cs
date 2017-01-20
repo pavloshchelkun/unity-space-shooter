62298a2ab7b736359933a84918a2b8d615b24576
Pavlo Shchelkun <pavel.shchelkun@gmail.com>
2017-01-20 14:53:05 +0200

    Add explosions to the scene when hazards or the player is destroyed

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,11 +2,24 @@
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject Explosion;
+    public GameObject PlayerExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+
+        if (Explosion != null)
+        {
+            Instantiate(Explosion, transform.position, transform.rotation);
+        }
+
+        if (other.tag == "Player")
+        {
+            Instantiate(PlayerExplosion, other.transform.position, other.transform.rotation);
         }
 
         Destroy(other.gameObject);
