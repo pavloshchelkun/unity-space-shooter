c6d37558f97ee507a2ef507fecca2fa264d5d37c
Pavlo Shchelkun <pavel.shchelkun@gmail.com>
2017-01-22 11:35:51 +0200

    Add start field and background scrolling

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float ScrollSpeed;
+    public float TileSizeZ;
+
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * ScrollSpeed, TileSizeZ);
+        transform.position = _startPosition + Vector3.forward * newPosition;
+    }
+}
